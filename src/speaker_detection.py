b41f66f69b6073c254ab2a33a4621b85fa8b5493
bngzl <bngzl@yahoo.com.sg>
2018-01-16 17:22:09 +0000

    Added speakerPosition and comments

--- src/speaker_detection.py
+++ src/speaker_detection.py
@@ -6,11 +6,7 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 
 cap = cv2.VideoCapture(0) 
 
-# HSV Color Sensitivity Ranges 
-# hul, huh = 147, 179 # Hue 
-# sal, sah = 61, 191 # Saturation
-# val, vah = 29, 205 # Value
-
+# Mask Colour Ranges:
 hul, huh = 119, 179 # Hue 
 sal, sah = 51, 243 # Saturation
 val, vah = 0, 255 # Value
@@ -26,6 +22,7 @@ while (True):
 	detected_status = "No Speaker Detected"
 	aspect_ratio_text = "Aspect Ratio Unavailable"
 	size_text = "Size Unavailable"
+	position_text = "Position Unavailable"
 
 	# Image Processing 
 	blurred = cv2.GaussianBlur(original_frame,(5,5),0)
@@ -42,37 +39,52 @@ while (True):
 											cv2.CHAIN_APPROX_SIMPLE
 											)
 	
-	# Sort contours by area 
+	# Sort contours with the 5 largest areas
 	contours = sorted(contours, key = cv2.contourArea, reverse= True)[:5]
 
 	# Loop over contours to find appropriate 
 	for c in contours: 
 		perimeter = cv2.arcLength(c, True)
-		approx = cv2.approxPolyDP(c, 0.1*perimeter, True)
-
-		if len(approx) >= 4 and len(approx) <=6: 
+		epsilon = 0.05*perimeter # Larger value allows for higher tolerances for bad shapes
+		approx = cv2.approxPolyDP(c, epsilon, True)
+ 
+		if len(approx) >= 4 and len(approx) <= 6: # Checks for number of vertices 
+		# Due to gaussian and motion blur, although looking for a square, may not actually get a square
 			(x,y,w,h) = cv2.boundingRect(approx) 
 			aspect_ratio = w/float(h) 
 
 			area = cv2.contourArea(c)
 			hullArea = cv2.contourArea(cv2.convexHull(c))
 			solidity = area/float(hullArea)
+			M = cv2.moments(c) 
 
+			# Calculate Center of Contour/Speaker's Position in Pixels
+			if M['m00']!=0: 
+				cX = int(M['m10']/M['m00'])
+				cY = int(M['m01']/M['m00'])
+			else: 
+				cX, cY = 0, 0
+
+			# To Calibrate: 
+			# https://docs.opencv.org/trunk/da/dc1/tutorial_js_contour_properties.html
 			keepSolidity = solidity > 0.9 
-			keepDims = w > 25 and h > 25
-			keepAspectRatio = aspect_ratio >= 0.5 and aspect_ratio <=1.7 
+			keepDims = w > 50 and h > 50 # Speaker should be larger than 50x50 pixels in room
+			keepAspectRatio = aspect_ratio >= 0.8 and aspect_ratio <=1.2 # Approximately square
+			speakerPosition = cX, cY 
 
 			if keepAspectRatio and keepSolidity and keepDims: 
-				cv2.drawContours(original_frame, [approx], -1, (0,0,255),2)
+				cv2.drawContours(original_frame, [approx], -1, (0,0,255),2) 
 				detected_status = "Speaker Detected"
-				aspect_ratio_text = str(aspect_ratio)
-				size_text = str(area) 
-
+				aspect_ratio_text = "Aspect Ratio: " + str(aspect_ratio)
+				size_text = "Area: " + str(area) 
+				position_text = "Speaker Position: " + str(speakerPosition) 
+				cv2.putText(original_frame, "x", speakerPosition, font, 0.5, (0, 255, 0), 1) 
 
 	# Display 
 	cv2.putText(original_frame, detected_status, (20,30), font, 0.5,(0,0,255), 1)
 	cv2.putText(original_frame, aspect_ratio_text, (20,60), font, 0.5,(255,0,0), 1)
 	cv2.putText(original_frame, size_text, (20, 90), font, 0.5, (0, 255, 0), 1)
+	cv2.putText(original_frame, position_text, (20, 120), font, 0.5, (255, 255, 255), 1)
 
 	cv2.imshow('Original', original_frame)
 	cv2.imshow('Edged', edged)
